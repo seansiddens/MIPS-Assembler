8898ed376fbec5c9f0f9d44adde5e235736bdf8f
Sean Siddens <seansiddens@gmail.com>
2021-02-22 22:13:38 -0800

    Basic assembler

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+// Editor settings
 var editor = ace.edit("editor");
 let source;
 
+// Set run button to disabled by default
+document.getElementById("run").disabled = true;
 
 // Buffers for memory and registers
 let REG = new Uint32Array(32);
@@ -10,20 +13,22 @@ let MEM = new Uint8Array(2**16);
 REG[0] = 0; // $zero
 let PC = 0; // Program counter
 
+let PROG_LENGTH; // Byte offset to track total # of instructions written to memory
+
 // Register definitions
 const REG_DEFS = {
-    "$zero": 0,
-    "$at": 1,
-    "$v0": 2, "$v1": 3,
-    "$a0": 4, "$a1": 5, "$a2": 6, "$a3": 7,
-    "$t0": 8, "$t1": 9, "$t2": 10, "$t3": 11, "$t4": 12, "$t5": 13, "$t6": 14,"$t7": 15,
-    "$s0": 16, "$s1": 17, "$s2": 18, "$s3": 19, "$s4": 20, "$s5": 21, "$s6": 22, "$s7": 23,
-    "$t8": 24, "$t9": 25,
-    "$k0": 26, "$k1": 27,
-    "$gp": 28,
-    "$sp": 29,
-    "$fp": 30,
-    "$ra": 31
+    "$zero": "00000",
+    "$at": "00001",
+    "$v0": "00010", "$v1": "00011",
+    "$a0": "00100", "$a1": "00101", "$a2": "00110", "$a3": "00111",
+    "$t0": "01000", "$t1": "01001", "$t2": "01010", "$t3": "01011", "$t4": "01100", "$t5": "01101", "$t6": "01110", "$t7": "01111",
+    "$s0": "10000", "$s1": "10001", "$s2": "10010", "$s3": "10011", "$s4": "10100", "$s5": "10101", "$s6": "10110", "$s7": "10111",
+    "$t8": "11000", "$t9": "11001",
+    "$k0": "11010", "$k1": "11011",
+    "$gp": "11100",
+    "$sp": "11101",
+    "$fp": "11110",
+    "$ra": "11111"
 };
 
 const MAX_PROGRAM_SIZE = 4096;   // Define allocation for program instructions
@@ -33,22 +38,43 @@ const MAX_STATIC = 2**15;   // Define allocation for static data
 const PROGRAM_TEXT_ADDR = 0
 const STATIC_ADDR = PROGRAM_TEXT_ADDR + MAX_PROGRAM_SIZE
 
-// Takes in opcode (as binary string) and corresponding instr command (as string) and adds it to the opcode map
-function addCode(opcodeStr, instr) {
-    let opcode = parseInt(opcodeStr, 2);
-    OPCODES[opcode] = instr;
+// Array of operation objects
+let OPS = [
+    {name: "add", type: 'R', args: 3, opcode: "100000"},
+    {name: "addi", type: 'I', args: 3, opcode: "001000"}
+]
+
+// Map of instruction keywords to their corresponding operation objects
+let INSTR_MAP = new Map();
+function createInstrMap() {
+    for (let i = 0; i < OPS.length; i++) {
+        INSTR_MAP.set(OPS[i].name, OPS[i]);
+    }
 }
-// Define instruction opcodes
-let OPCODES = {};
-addCode("100000", "add");
-addCode("001000", "addi");
+createInstrMap();
 
+// Map of opcodes to their corresponding operation objects
+let OPCODE_LKUP = new Map();
+function createOpcodeMap() {
+    for (let i = 0; i < OPS.length; i++) {
+        INSTR_MAP.set(OPS[i].opcode, OPS[i]);
+    }
+}
+createOpcodeMap();
 
+// Failure flag for assembler
+let ASSEMBLER_FAILED;
 function assemble() {
+    ASSEMBLER_FAILED = false; // Inititialized to false
+
+    // No instructions written yet
+    PROG_LENGTH = 0;
     // Set program counter to 0
     PC = 0;
+
     // Store source code in array, split line by line
     source = editor.getValue().split("\n"); 
+
     // Remove whitespace from either side of string
     for (let i = 0; i < source.length; i++) {
         source[i] = source[i].trim();
@@ -56,16 +82,17 @@ function assemble() {
 
     let text_flag = true;  // If true, current line is within .text field
     let data_flag = false; // If true, current line is within .data field
-    let instr;             // 32 bit translated machine instruction
-    let opcode;            // 6 bit binary opcode
+
+    let instr;             // 32 bit translated machine instruction - eventually written to memory
     let line;              // Array for storing line of source code separated by spaces
 
     // Destination registers
-    let rd = 0;
+    let rd;
     // Source operand registers
-    let rs = 0;
-    let rt = 0;
-    let imm = 0;
+    let rs;
+    let rt;
+    // Immediate value
+    let imm;
 
     // Traverse code and translate to program instructions
     for (let i = 0; i < source.length; i++) {
@@ -90,50 +117,112 @@ function assemble() {
 
         // Process .text instructions
         if (text_flag) {
-            console.log(line);
-            switch (line[0]) {
-                case "add":
-                    opcode = Number(getKey(OPCODES, "add"));
+
+            // Check for system call
+            if (line[0] === "syscall") {
+                // Switch on value of $v0
+                switch (REGS[2]) {
+                    case 1:
+                        console.log("syscall!");
+                        break;
+                }
+            }
+            // Check if valid instruction keyword
+            else if (INSTR_MAP.has(line[0])) {
+                let instr_obj = INSTR_MAP.get(line[0]);
+                console.log(instr_obj)
+
+                if (line.length > instr_obj.args + 1) {
+                    // Too many arguments provided - print error
+                    assemblerErr(i);
+                    break;
+                }
+
+                if (instr_obj.type === 'R') {
+                    // Ensure proper args
                     rd = line[1] in REG_DEFS ? line[1] : assemblerErr(i);
                     rs = line[2] in REG_DEFS ? line[2] : assemblerErr(i);
                     rt = line[3] in REG_DEFS ? line[3] : assemblerErr(i);
 
-                    instr = "000000" + REG_DEFS[rs].toString(2) + REG_DEFS[rt].toString(2) + REG_DEFS[rd].toString(2) + "000000" + opcode.toString(2);
+                    instr = instr_obj.opcode + REG_DEFS[rs] + REG_DEFS[rt] + REG_DEFS[rd] + "00000" + "000000";
                     writeInstruction(instr);
+                    // console.log(instr);
 
-                    console.log(parseInt(instr, 2).toString(16).toUpperCase());
-
-                    break;
-                case "addi":
-                    opcode = getKey(OPCODES, "addi");
-                    rd = line[1] in REG_DEFS ? line[1] : assemblerErr(i);
+                    // for (let i = 0; i < 4; i++) {
+                    //     let foo = MEM[i].toString(2);
+                    //     console.log("0".repeat(8-foo.length) + foo);
+                    // }
+                }
+                else if (instr_obj.type === 'I') {
+                    // Ensure proper args
+                    rt = line[1] in REG_DEFS ? line[1] : assemblerErr(i);
                     rs = line[2] in REG_DEFS ? line[2] : assemblerErr(i);
-                    imm = !isNaN(line[3]) ? line[3] : assemblerErr(i);
-                    console.log(rd, rs, imm);
-                    
-                    break;
+                    imm = !isNaN(line[3]) ? parseInt(line[3]).toString(2) : assemblerErr(i);
+                    imm = "0".repeat(16-imm.length) + imm; // Extend bits to 16 width
+
+                    instr = instr_obj.opcode + REG_DEFS[rs] + REG_DEFS[rt] + imm;
+                    writeInstruction(instr);
+                    // console.log(instr);
+                    // console.log(instr.length);
+                }
+
+
+            }
+            else {
+                // Invalid isntruction keyword - print error
+                return assemblerErr(i);
             }
         }
     }
 
+    // If assembler succeeded, enable run button
+    if (!ASSEMBLER_FAILED && PROG_LENGTH > 0) {
+        document.getElementById("run").disabled = false;
+    }
 } // End assemble()
 
+function run() {
+    // Set program counter to 0
+    PC = 0;
+
+    // Instruction bitstring
+    let instr;
+    // Instruction execution loop
+    while(PC <= PROG_LENGTH) {
+
+        // Get instruction memory
+        instr = MEM[PC+3].toString(2) + MEM[PC+2].toString(2) + MEM[PC+1].toString(2) + MEM[PC].toString(2);
+        console.log(instr);
+
+        // Increment PC after fetching instruction
+        PC += 4;
+
+    }
+
+}
+
 // Write a given 32 bit instruction to memory address pointed to by the program counter
+// Little Endian data storage
+// 4|        | < -- PC + 4 (next instruction)
+// 3|  MSBs  |
+// 2|xxxxxxxx|   
+// 1|xxxxxxxx|   
+// 0|  LSBs  | <-- PC
 function writeInstruction(instr) {
-    MEM[PC] = parseInt(instr.slice(0, 7), 2);
+    // Keep track of total # of instructions written, each instruction is 4 bytes
+    PROG_LENGTH += 4;
+    // LSBs are the rightmost bits in the instruction string
+    MEM[PC] = parseInt(instr.slice(24, 31), 2);
+    PC++;
+    MEM[PC] = parseInt(instr.slice(16, 23), 2);
     PC++;
     MEM[PC] = parseInt(instr.slice(8, 15), 2);
     PC++;
-    MEM[PC] = parseInt(instr.slice(16, 23));
+    MEM[PC] = parseInt(instr.slice(0, 7), 2);
     PC++;
-    MEM[PC] = parseInt(instr.slice(24, 31));
-    PC++;
-}
-
-function getKey(object, value) {
-    return Object.keys(object).find(key => object[key] === value);
 }
 
 function assemblerErr(lineNumber) {
+    ASSEMBLER_FAILED = true;
     console.log("Assembler errror on line ", lineNumber + 1);
 }
